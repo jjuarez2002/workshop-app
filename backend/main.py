46cd229e49581e88d15070004bd9f0bfd372040d
Rayyanisacrayon <rayyanzamir@gmail.com>
2022-04-09 15:36:19 -0500

    ws3 catchup2

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,42 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastApi()
+app = FastAPI()
 
-@app.get("/")
 # localhost:xxxx
+@app.get("/")
 def root():
-    {"hello":"world"}
+    return {"hello":"world"}
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+
+# create an endpoint that recieves form data for a single service
+
+@app.post('/submit-request')
+async def submitRequest(req:Request):
+    # service type (inspection or repair) - serviceName
+    # requested time (text string)        - serviceTime
+    # comments (text string)              - comments
+
+    body = await req.json()
+    # body = pythong dict w key value
+    # serviceName = body['ServiceName']
+    serviceName = body.get('ServiceName')
+    serviceTime = body.get('ServiceTime')
+    comments = body.get('comments')
+
+    # store this somewhere
+
+    print("serviceName", serviceName)
+    print("serviceTime", serviceTime)
+    print("comments", comments)
+
+    return body
